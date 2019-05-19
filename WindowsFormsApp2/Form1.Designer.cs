e03ccfd03a0b9e5c1a3b7f218f57d6753d95eaf6
rex <rex@GSTTS>
2019-05-20 01:50:58 +0300

    Добавьте файлы проекта.

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pBox = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pBox
+            // 
+            this.pBox.BackColor = System.Drawing.Color.White;
+            this.pBox.Location = new System.Drawing.Point(12, 12);
+            this.pBox.Name = "pBox";
+            this.pBox.Size = new System.Drawing.Size(490, 634);
+            this.pBox.TabIndex = 0;
+            this.pBox.TabStop = false;
+            this.pBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pbCanvas_Paint);
+            this.pBox.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pbCanvas_MouseDown);
+            this.pBox.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pbCanvas_MouseMove);
+            this.pBox.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pbCanvas_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(665, 357);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 187);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "CLEAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(829, 623);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "LINE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.ForeColor = System.Drawing.Color.Blue;
+            this.radioButton1.Location = new System.Drawing.Point(519, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(46, 17);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Blue";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(519, 35);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(14, 13);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(928, 658);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        Point lastcoords = Point.Empty;
+        bool isPressed;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pBox.Paint += new PaintEventHandler(pbCanvas_Paint);
+            pBox.MouseDown += new MouseEventHandler(pbCanvas_MouseDown);
+            pBox.MouseMove += new MouseEventHandler(pbCanvas_MouseMove);
+            pBox.MouseUp += new MouseEventHandler(pbCanvas_MouseUp);
+        }
+        bool allowedToPaint = new bool();
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            allowedToPaint = false;
+            pBox.Image = null;
+            pBox.Invalidate();
+        }
+
+
+        private void pbCanvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            lastcoords = e.Location;
+            isPressed = true;
+           
+        }
+        
+        private void pbCanvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(isPressed)
+            {
+                if (lastcoords != null)
+                {
+                    if (pBox.Image == null)
+                    {
+                        Bitmap bitmap = new Bitmap(pBox.Width, pBox.Height);
+                        pBox.Image = bitmap;
+                    }
+                    Graphics g = Graphics.FromImage(pBox.Image);
+                    if (radioButton1.Checked)
+                    {
+                        data data = new data();
+                        string hexBlue = "#FF0000FF";
+                        data.penColor = Int32.Parse(hexBlue.Replace("#", ""), System.Globalization.NumberStyles.HexNumber);
+                    }
+                    Pen pen = new Pen(Color.FromArgb(data.penColor), 2);
+                    g.DrawLine(pen, lastcoords, e.Location);
+                    g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                    pen.Dispose();
+                    pBox.Invalidate();
+                    lastcoords = e.Location;
+                } 
+            } 
+        }
+
+        private void pbCanvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            lastcoords = Point.Empty;
+            isPressed = false;
+        }
+
+        private void pbCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            if(allowedToPaint)
+            {
+                Bitmap rectmap = new Bitmap(pBox.Width, pBox.Height);
+                pBox.Image = rectmap;
+                Graphics f = Graphics.FromImage(pBox.Image);
+                Pen pen = new Pen(Color.Blue, 2);
+                for (int i = 0; i <= pBox.Height; i += 15)
+                {
+                    f.DrawLine(pen, new Point(0, i), new Point(1000, i));
+                }
+                for (int i = 0; i <= pBox.Width; i += 15)
+                {
+                    f.DrawLine(pen, new Point(i, 0), new Point(i, 1000));
+                }
+                f.Dispose();
+                pen.Dispose();
+                pBox.Invalidate();
+                allowedToPaint = false;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            allowedToPaint = true;
+            pBox.Invalidate();
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            pBox.Invalidate();
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            pBox.Invalidate();
+        }
+    }
+}
